3057bea22c6e8052dde49090a89e5c49cb3559e1
YRodrigo-Ruiz <rod_ruiz@hotmail.com.ar>
2022-12-13 14:08:33 -0300

    agregando index.html

--- app.js
+++ app.js
@@ -4,13 +4,10 @@ const app = express ();
 
 const path = require("path")           
 
-app.listen(3000, () => 
-    console.log("servidor a las escucha del puerto 3000"));    
+app.listen(3000, () => {
+    console.log("servidor a las escucha del puerto 3000");
+});    
 
 app.get("/", (req, res) => {
-    res.send("Hello Word");
-});                                 
-
-    app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./views/index.html"));
+    res.sendFile(path.join(__dirname, "./Views/index.html"));
 });
