ca3fe266eb1e7808922d688b6258fe09a9ec45cf
YRodrigo-Ruiz <rod_ruiz@hotmail.com.ar>
2022-12-14 10:37:13 -0300

    rutas en app.js

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express ();
 
 const path = require("path")           
 
-app.listen(3000, () => {
-    console.log("servidor a las escucha del puerto 3000");
-});    
+app.listen(3001, () => {
+    console.log("servidor a las escucha del puerto 3001");
+}); 
+
+const publicPath = path.resolve(__dirname, "./Public");
+app.use( express.static(publicPath) );
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./Views/index.html"));
